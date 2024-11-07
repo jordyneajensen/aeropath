1212e68a39f6b2631d338044e7f8c0adfee13306
Jordyn Jensen <jordyneajensen@gmail.com>
2024-11-07 07:44:44 -0600

    Set up Devise with User model and custom fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_07_122625) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_07_134208) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,5 +28,22 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_07_122625) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["registration"], name: "index_aircraft_on_registration", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "pilot_license_number"
+    t.string "role", default: "pilot"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["pilot_license_number"], name: "index_users_on_pilot_license_number", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
